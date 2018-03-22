e3ddf7fdc4511386f08b00191f5e03b1bfa067b7
RaviTeja Gangineni <31611594+gravity09@users.noreply.github.com>
2018-03-22 17:56:47 +0530

    Delete test_sample.py

--- tests/test_sample.py
+++
@@ -1,8 +0,0 @@
-from /home/ubuntu/starter/project import addition
-from /home/ubuntu/starter/project import multiplication
-
-def test_addition():
-	assert addition(4,5) == 9
-
-def test_multiplication():
-	assert multiplication(4,5) == 20
